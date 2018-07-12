a45580b1199edd85346633cdd94777c2ce47a441
krzysztof <krzysztof@flowlab.no>
2018-07-12 12:29:51 +0200

    Brew formula update for flow version v0.1.0

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -3,7 +3,7 @@ class Flow < Formula
   homepage "https://www.flowlab.no/"
   url "https://github.com/flow-lab/flow/releases/download/v0.1.0/flow_0.1.0_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.1.0"
-  sha256 "61cd39249cfd2320e646ab9e2adb94533e3354bec4c2c8bde5b18689685ce3c5"
+  sha256 "9c09f292d4e402ff4b84289b12b9c73f3ff72b83ce8f1539ea9d659c0ef9149b"
 
   def install
     bin.install "flow"

cd5ef3abfb57d6ceeef3a7eea96d6ecb2cbf1d33
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-23 22:16:29 +0200

    Brew formula update for flow version v0.1.12

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,9 +1,9 @@
 class Flow < Formula
   desc "AWS tooling for faster development."
   homepage "https://www.flowlab.no/"
-  url "https://github.com/flow-lab/flow/releases/download/v0.1.11/flow_0.1.11_darwin_amd64.tar.gz"
-  version "0.1.11"
-  sha256 "4a87ecc0cf1168ea3b1b405cc706c1dd63a5ce96f494d6975d9cdc363e534b8c"
+  url "https://github.com/flow-lab/flow/releases/download/v0.1.12/flow_0.1.12_darwin_amd64.tar.gz"
+  version "0.1.12"
+  sha256 "48b1088a0342243c48dfcb23e66717be1bb03602998c73b4c5b3687b588ba00e"
 
   def install
     bin.install "flow"
